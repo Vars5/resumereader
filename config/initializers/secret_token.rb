e261bc5e516a6e32eced603c255caaa195eda243
Pasha Rayan <pasha.rayan@gmail.com>
2013-03-19 22:11:24 +1100

    create initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resumereader::Application.config.secret_token = '53ccd7b792ef3de9b94e981d2b98beb001b8abdbaff031330f69d69c6459d8dc091bd46d3ee71218e1a891c6ef12cf3c8602f3884a0f33111178a3a723e945e9'
