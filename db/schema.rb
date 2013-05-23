f5a0a9bb66d60730fb2d76c54110899b14ab78f4
VC <varun.chhabra91@gmail.com>
2013-05-24 00:22:50 +1000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130521151956) do
-=======
-
+ActiveRecord::Schema.define(:version => 20130521231556) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
