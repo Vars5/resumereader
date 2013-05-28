748c1f76059a159fc7c2295d7638ce9508972522
VC <varun.chhabra91@gmail.com>
2013-05-28 16:23:07 +1000

    changed bucket and added credentials

--- app/controllers/app_lists_controller.rb
+++ app/controllers/app_lists_controller.rb
@@ -2,6 +2,7 @@ class AppListsController < ApplicationController
 
   before_filter :authenticate_user!
 
+
   def create
     @appList = current_user.app_lists.build(params[:applist])
     @job = Job.find_by_id(params[:applist][:job_id])
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+Paperclip::Attachment.default_options[:url] = 's3-website-ap-southeast-2.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
