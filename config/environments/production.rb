0575fa50dfee9e733d4f1214678e9ff3534545b6
VC <varun.chhabra91@gmail.com>
2013-05-28 17:25:35 +1000

    changed port, removed comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,10 +16,10 @@ Resumereader::Application.configure do
   
     config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
-      :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+      :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
     #  :enable_starttls_auto => true, # detects and uses STARTTLS
       :user_name => ENV["MANDRILL_USERNAME"],
-      :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+      :password  => ENV["MANDRILL_PASSWORD"] # SMTP password is any valid API key
      # :authentication => 'login', # Mandrill supports 'plain' or 'login'
      #  :domain => 'resumereader.herokuapp.com', # your domain to identify your server when connecting
     }
