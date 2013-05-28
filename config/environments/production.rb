da33a9e58547967ffae72f5fa4e19473e91495d9
VC <varun.chhabra91@gmail.com>
2013-05-28 17:13:30 +1000

    mandrill test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,17 +12,16 @@ Resumereader::Application.configure do
     }
   }
   
-  
   # Mandrill settings
   
     config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
       :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
-      :enable_starttls_auto => true, # detects and uses STARTTLS
+    #  :enable_starttls_auto => true, # detects and uses STARTTLS
       :user_name => ENV["MANDRILL_USERNAME"],
       :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
-      :authentication => 'login', # Mandrill supports 'plain' or 'login'
-      :domain => 'resumereader.herokuapp.com', # your domain to identify your server when connecting
+     # :authentication => 'login', # Mandrill supports 'plain' or 'login'
+     #  :domain => 'resumereader.herokuapp.com', # your domain to identify your server when connecting
     }
   
   # Code is not reloaded between requests
