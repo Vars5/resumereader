715178bdaff2ac48213b87e059a5ea379b03ba06
VC <varun.chhabra91@gmail.com>
2013-05-28 16:41:42 +1000

    change to US bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Resumereader::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'resume-test',
+      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
