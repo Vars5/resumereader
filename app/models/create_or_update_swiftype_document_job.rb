2459c3ad9dcbda1127e0e73d01a1634ae9d028dd
Pasha Rayan <pasha.rayan@gmail.com>
2013-06-13 15:19:07 +1000

    changes to swiftype:

--- app/models/create_or_update_swiftype_document_job.rb
+++ app/models/create_or_update_swiftype_document_job.rb
@@ -1,7 +1,7 @@
 class CreateOrUpdateSwiftypeDocumentJob < Struct.new(:job_id)
   def perform
     job = Job.find(job_id)
-    url = Rails.application.routes.url_helpers.job_url(job, :host =>"localhost:3000")
+    url = Rails.application.routes.url_helpers.job_url(job)
     client = Swiftype::Easy.new
     client.create_or_update_document(ENV['SWIFTYPE_ENGINE_SLUG'],
                                      Job.model_name.downcase,
