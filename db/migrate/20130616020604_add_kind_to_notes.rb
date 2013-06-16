4d608c4b384c9ac2ee9609e52691fa6c3ad09f07
pasharayan <pasha.rayan@gmail.com>
2013-06-16 12:26:32 +1000

    Update 20130616020604_add_kind_to_notes.rb

--- db/migrate/20130616020604_add_kind_to_notes.rb
+++ db/migrate/20130616020604_add_kind_to_notes.rb
@@ -1,7 +1,6 @@
 class AddKindToNotes < ActiveRecord::Migration
   def change
     add_column :notes, :kind, :string
-    remove_column :app_lists, :kind, :string
     remove_column :notes, :type, :string    
     Note.update_all ["kind = ?", "comment"]
   end
