acb719c19224eed9791f88c61867dc6942f327a0
VC <varun.chhabra91@gmail.com>
2013-07-04 15:30:34 +1000

    modified search migration

--- db/migrate/20130704051641_add_integers_to_searches.rb
+++ db/migrate/20130704051641_add_integers_to_searches.rb
@@ -2,7 +2,5 @@ class AddIntegersToSearches < ActiveRecord::Migration
   def change
     add_column :searches, :role_id, :integer
     add_column :searches, :category_id, :integer
-    remove_column :searches, :discipline, :string
-    remove_column :searches, :role, :string
-  end
+    end
 end
