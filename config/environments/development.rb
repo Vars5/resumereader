01bf2c74030d2eef6a22171989b76008523a458a
VC <varun.chhabra91@gmail.com>
2013-07-09 18:17:21 +1000

    added letter_opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,7 @@ Resumereader::Application.configure do
   
   config.action_mailer.default_url_options = { :host => 'localhost' }
   
-
+=begin
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
@@ -61,9 +61,9 @@ Resumereader::Application.configure do
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD']
   }
+=end
 
-
- config.action_mailer.delivery_method = :smtp
+ config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
