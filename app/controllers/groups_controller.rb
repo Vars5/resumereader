0e1818e2e2fa81018e6a26ccb5a4d5e574ed89dd
VC <varun.chhabra91@gmail.com>
2013-07-17 17:14:57 +1000

    nested form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,27 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+    @group.groupmembers.build
+  end
+
+  def create
+    @group = Group.new(params[:group])
+    @group.save
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
+  def update
+    @group = Group.update_attributes(params[:id])
+  end
+  
   def show
+    @group = Group.find(params[:id])
   end
 
   def index
+    @groups = Groups.all
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,10 @@
 class Group < ActiveRecord::Base
-  attr_accessible :name, :private
+  attr_accessible :name, :private, :groupmembers_attributes
 
   #social _feature_
   has_many :groupmembers
   has_many :users, through: :groupmembers
+  accepts_nested_attributes_for :groupmembers
 
 
 end
