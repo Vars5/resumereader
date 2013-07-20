ace6d73c437c8775b918d24484643f54bdf49090
VC <varun.chhabra91@gmail.com>
2013-07-20 20:27:47 +1000

    modify s3 details

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,12 +64,11 @@ class User < ActiveRecord::Base
   has_attached_file :avatar,
       :storage => :s3,
       :bucket => 'resquery-avatars',
-#      :url  => ":s3_sydney_url",
       :url => ":s3_domain_url",
       :path => "/:class/avatars/:id_:basename.:style.:extension",
       :s3_credentials => {
-        :access_key_id => 'AWS_ACCESS_KEY_ID',
-        :secret_access_key => 'AWS_SECRET_ACCESS_KEY'
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
       }
 
   def groups_list
