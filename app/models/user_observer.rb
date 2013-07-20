40209cfbf49a0409bba912e25425615a906e5a1d
VC <varun.chhabra91@gmail.com>
2013-07-20 22:36:49 +1000

    fix part 2

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,9 +1,8 @@
 class UserObserver < ActiveRecord::Observer 
   
     def after_create(user)
-      if user.sign_in_count > 0
+      if user.invitation_token.nil?
         UserMailer.signup_confirmation(user).deliver
       end
     end  
-  
 end
