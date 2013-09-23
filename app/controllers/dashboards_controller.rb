9dfda5e203a0bdf6bda18db01bc23af3acd2306b
pasharayan <pasha.rayan@gmail.com>
2013-09-23 21:39:05 +1000

    analytics fix

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,11 +4,8 @@ class DashboardsController < ApplicationController
   
   def dashboard
     @appListCount = current_user.app_list_count
-    @appList = current_user.app_lists.find(:all, :joins => :job, :order => "due_date DESC")
-    
+    @appList = current_user.app_lists.find(:all, :joins => :job, :order => "due_date DESC")    
     @category = Category.order("discipline")
-
-    
   end
   
   
