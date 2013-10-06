613a9cc073ad11ea4bbd90216e2329ee12c2d5e9
pasharayan <pasha.rayan@gmail.com>
2013-10-06 11:35:10 +1100

    test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -53,4 +53,9 @@ class Comment < ActiveRecord::Base
   def self.find_commentable(commentable_str, commentable_id)
     commentable_str.constantize.find(commentable_id)
   end
+  
+  def get_company_for_comment
+    Company.find(self.commentable_id)
+  end
+  
 end
