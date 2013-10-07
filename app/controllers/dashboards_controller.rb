74cdabf2ad42d5cb57f082361bd6bb73cd6bce9a
pasharayan <pasha.rayan@gmail.com>
2013-10-07 23:09:30 +1100

    test

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,7 +6,6 @@ class DashboardsController < ApplicationController
     @appListCount = current_user.app_list_count
     @appList = current_user.app_lists.find(:all, :joins => :job, :order => "due_date DESC")    
     @category = Category.order("discipline")
-    @company = Company.all( :limit => 5)
     @comments = Comment.order("created_at DESC")
   end
   
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def full_title(page_title)
-      base_title = "ResumeQuery"
+      base_title = "DemPoints"
       if page_title.empty?
         base_title
       else
