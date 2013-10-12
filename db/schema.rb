48d483e979d2b0e3d29e6534ce4fd59220df90e2
pasharayan <pasha.rayan@gmail.com>
2013-10-12 15:30:52 +1100

    styling and form changes

--- db/schema.rb
+++ db/schema.rb
@@ -34,31 +34,6 @@ ActiveRecord::Schema.define(:version => 20131011142635) do
     t.text     "tagline"
   end
 
-  create_table "blog_comments", :force => true do |t|
-    t.string   "name",       :null => false
-    t.string   "email",      :null => false
-    t.string   "website"
-    t.text     "body",       :null => false
-    t.integer  "post_id",    :null => false
-    t.string   "state"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "blog_comments", ["post_id"], :name => "index_blog_comments_on_post_id"
-
-  create_table "blog_posts", :force => true do |t|
-    t.string   "title",                         :null => false
-    t.text     "body",                          :null => false
-    t.integer  "blogger_id"
-    t.string   "blogger_type"
-    t.integer  "comments_count", :default => 0, :null => false
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-  end
-
-  add_index "blog_posts", ["blogger_type", "blogger_id"], :name => "index_blog_posts_on_blogger_type_and_blogger_id"
-
   create_table "boards", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -78,8 +53,8 @@ ActiveRecord::Schema.define(:version => 20131011142635) do
 
   create_table "categories", :force => true do |t|
     t.string   "discipline"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "name"
     t.string   "category"
   end
@@ -299,6 +274,8 @@ ActiveRecord::Schema.define(:version => 20131011142635) do
   end
 
   create_table "searches", :force => true do |t|
+    t.string   "role"
+    t.string   "discipline"
     t.string   "location"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
@@ -312,23 +289,6 @@ ActiveRecord::Schema.define(:version => 20131011142635) do
     t.string   "forum_name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-  create_table "taggings", :force => true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context"
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
-
-  create_table "tags", :force => true do |t|
-    t.string "name"
   end
 
   create_table "users", :force => true do |t|
