3d6e67148757e11d7b01e859a40242bdde851c8c
pasharayan <pasha.rayan@gmail.com>
2014-01-01 09:27:05 +1100

    added category name to api'

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,7 +7,7 @@ class ApiController < RocketPants::Base
     allComments.each do |comment|
       #note, for api, we currently use 'subject' to load the company's name
       #set it so we can show the companies name on the phone
-      comment.title = "#{comment.get_company_for_comment.name} - #{comment.subject}"
+      comment.title = "#{comment.get_company_for_comment.name} - #{Category.find(comment.category_id).name}"
     end
     expose allComments
   end
