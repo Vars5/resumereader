7ade11af62e998fc0ddae61a33953c8892d3777f
pasharayan <pasha.rayan@gmail.com>
2014-02-12 08:10:19 +1100

    removed buggy onboarding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :new_question
-  
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
   if Rails.env.production?
     rescue_from CanCan::AccessDenied do |exception|
         redirect_to root_url, :alert => exception.message
     end
   end 
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u| 
+      u.permit(:password, :password_confirmation, :current_password) 
+    }
+  end
+
 
   #after_filter response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
   #after_filter response.headers["Pragma"] = "no-cache"
