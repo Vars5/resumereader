dc1cff151dd47ca882d3306d22b5ef7bf7372173
pasharayan <pasha.rayan@gmail.com>
2014-02-12 22:44:37 +1100

    fixed sign in bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,7 @@ class ApplicationController < ActionController::Base
   end 
 
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) { |u| 
-      u.permit(:password, :password_confirmation, :current_password) 
-    }
-  end
+
 
 
   #after_filter response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
